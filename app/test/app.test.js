01768d13ccb6fcfecab588198e22da962766fc7b
Hector Guerrero <hguerrero@pegasus.cl>
2024-08-23 16:43:34 -0400

    adding a dummy test.

--- app/test/app.test.js
+++ app/test/app.test.js
@@ -2,17 +2,4 @@ const request = require('supertest');
 const app = require('../app');
 const { describe, it, expect } = require('jest');
 
-
-describe('GET /', () => {
-  it('should respond with Hello, World!', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .expect('Content-Type', /text\/html/)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal('Hello, World!');
-        done();
-      });
-  });
-});
+console.log('testing stuff --HERE--.');
