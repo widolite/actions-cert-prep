1f26b1a75e5718d226d62e9271b9bfb5b8cb5936
Hector Guerrero <hguerrero@pegasus.cl>
2024-08-23 16:25:17 -0400

    set simple app

--- app/app.js
+++ app/app.js
@@ -1,24 +1,12 @@
-const config = require('./config');
 const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-
-const index = require('./routes/index');
-const who = require('./routes/who');
-const contact = require('./routes/contact');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.set('port', config.port);
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-app.use('/', express.static('public'))
-app.use('/', index);
-app.use('/who', who);
-app.use('/contact', contact);
-
-app.listen(config.port, () => {
-  console.log(`Demo app is running on ${config.port}!`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
---
+++ app/test/app.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /', () => {
+  it('should respond with Hello, World!', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', /text\/html/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.equal('Hello, World!');
+        done();
+      });
+  });
+});
